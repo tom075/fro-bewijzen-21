a4a921409830f9140b8514b43da898ac57b9bc57
Tom Folkers <24825@ma-web.nl>
2018-10-01 21:52:45 +0200

    fro bem

---
+++ js/bootcamp/deel1/script.js
@@ -0,0 +1,24 @@
+// "use strict";
+var vak = document.getElementById("titel");
+var opdracht = document.getElementById("opdracht");
+var informatie = document.getElementById("text");
+var naam = document.getElementById("naam");
+var klas = document.getElementById("klas");
+var copyright = document.getElementById("copy");
+
+var hetVak = "FRO";
+var deOpdracht = "opdracht1";
+var deInformate = "informatie tekst enzo";
+var mijnNaam = "Tom Folkers";
+var mijnKlas = "MD2A";
+var Copyright = "© copyright 2018 | Tom Folkers";
+
+function verander(){
+  vak.innerHTML = hetVak;
+  opdracht.innerHTML = deOpdracht;
+  informatie.innerHTML = deInformate;
+  naam.innerHTML = mijnNaam;
+  klas.innerHTML = mijnKlas;
+  copyright.innerHTML = Copyright;
+
+}
